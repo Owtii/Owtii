f03be7e89d64fbc6da01083ea895258d16b7cb66
papsmoke <fortnitesbasement@gmail.com>
2023-08-06 02:59:36 +0200

    yeye

---
+++ src/Pages/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/home.css";
+
+function Home() {
+  return <div className="text">In maintenance...</div>;
+}
+
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Logo from "../assets/74984.png";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="leftSide"></div>
+      <div className="rightSide"></div>
+    </div>
+  );
+}
+
+export default Navbar;
