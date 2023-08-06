c211f9ca03221d081ff68ac3d240c875f581d890
papsmoke <fortnitesbasement@gmail.com>
2023-08-06 03:01:41 +0200

    huh

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/home.css";
 
 function Home() {
-  return <div className="text">In maintenance...</div>;
+  return <div className="text">In maintenance....</div>;
 }
 
 export default Home;
